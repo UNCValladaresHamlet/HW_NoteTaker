96ed9cd2c0c9d9b7d20f3ca8c32fe9b559e20462
Hamlet Valladares <sigorahv@gmail.com>
2021-06-15 15:25:07 -0400

    added features

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,20 +1,20 @@
-const notes = require('../db/db.json');
+const noteInfo = require('../db/db.json');
 const fs = require('fs');
 // ROUTING
 
 module.exports = (app) => {
 
-  app.get('/api/notes', (req, res) => res.json(notes));
+  app.get('/api/notes', (req, res) => res.json(noteInfo));
 
 
   app.post('/api/notes', (req, res) => {
 
-    notes.push(req.body);
+    noteInfo.push(req.body);
     console.log('req.body: ', req.body)
 
-    fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err) =>
+    fs.writeFile('./Develop/db/db.json', JSON.stringify(noteInfo), (err) =>
       err ? console.error(err) : console.log('Hamlet look here'))
-    res.json(notes);
+    res.json(noteInfo);
   });
 
 };
