cb3deac5414b0a9508242dbcba6474e22ef84936
Hamlet Valladares <sigorahv@gmail.com>
2021-06-15 01:17:45 -0400

    got it to work

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,11 +1,22 @@
-const notes = require("../db/db.json")
+const notes = require('../db/db.json');
+const fs = require('fs');
+// ROUTING
 
 module.exports = (app) => {
 
-  app.get('/api/tables', (req, res) => res.json(notes));
+  app.get('/api/notes', (req, res) => res.json(notes));
 
-   app.post('/api/notes', (req, res) => {
 
+  app.post('/api/notes', (req, res) => {
+
+    notes.push(req.body);
+    console.log('req.body: ', req.body)
+
+    fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err) =>
+      err ? console.error(err) : console.log('Hamlet look here'))
+    res.json(notes);
   });
 
 };
+
+
